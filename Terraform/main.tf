d72092675efe3d4ab99fc2fa74406a792459ed03
Abhi <abhin032@gmail.com>
2024-03-20 00:08:19 +0530

    Pushing terraform files

---
+++ Terraform/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.18.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "D:\\DE\\terrademo\\keys\\my_creds.json"
+  project     = var.Project_ID
+  region      = var.Region
+}
+
+
+resource "google_storage_bucket" "zomato-bucket" {
+  name          = "zomato_bucket_${local.current_time}"
+  location      = var.Region
+  storage_class = var.Storage_class
+  force_destroy = true
+  public_access_prevention = var.Public_Acess
+  versioning {
+    enabled = "true"
+  }
+}
+
+
+resource "google_bigquery_dataset" "zomato-dataset-bq" {
+  dataset_id = var.BQ_Dataset
+  project = var.Project_ID
+  location = var.Region
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,34 @@
+locals {
+  current_time = formatdate("YYYYMMDDhhmmss", timestamp())
+}
+
+variable "BQ_Dataset" {
+  description = "The name of the BigQuery table the data will be written to."
+  default     = "zomato"
+  type        = string
+}
+
+variable "Region" {
+  description = "The location where the resources should be created."
+  default     = "asia-south1"
+  type        = string
+}
+
+variable "Project_ID" {
+  description = "Project ID of the project"
+  default     = "zomato-zoomcamp"
+  type        = string
+}
+
+variable "Storage_class" {
+  description = "The storage class for the bucket to be created."
+  default     = "STANDARD"
+  type        = string
+}
+
+variable "Public_Acess" {
+  description = "Public access to be inherited or enforced"
+  default     = "enforced"
+  type        = string
+}
+
